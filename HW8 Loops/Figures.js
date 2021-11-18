d498a3ad8aabfb85888cb2006754f6cd1f1b3907
Володимир Сидор <vovansw@ukr.net>
2021-11-18 17:07:31 +0200

    HW8 Loops

---
+++ HW8 Loops/Figures.js
@@ -0,0 +1,58 @@
+// Rectangle
+// let i = 0;
+// while (i < 10) {
+//     console.log('**********');
+//     i++;
+// }
+
+// Square
+// let i = 0;
+// while (i < 10) {
+//     console.log('*  *  *  *  *  *  *  *  *  *');
+//     i++;
+// }
+
+// Triangle 1
+// let result = '';
+// for (let i = 0; i < 10; i++) {
+//     result = result + '*';
+//     console.log(result);
+// }
+
+// Triangle 2
+// let result = '';
+// for (let i = 11; i >= 2; i--) {
+//     for (let a = i - 1; a >= 1; a--) {
+//         result = result + '*';
+//     }
+//     console.log(result);
+//     result = '';
+// }
+
+// Triangle 3
+// let result = '';
+// for (let i = 10; i >= 1; i--) {
+//     for (let a = i - 1; a >= 1; a--) {
+//         result = result + ' ';
+//     }
+//     for (let b = 11 - i; b >= 1; b--) {
+//         result = result + '*';
+//     }
+//     console.log(result);
+//     result = '';
+// }
+
+// Triangle 4 
+// let result = '';
+// for (let i = 11; i >= 2; i--) {
+//     for (let b = 11 - i; b >= 1; b--) {
+//         result = result + ' ';
+//     }
+//     for (let a = i - 1; a >= 1; a--) {
+//         result = result + '*';
+//     }
+//     console.log(result);
+//     result = '';
+// }
+
+
