342fd816300b4b7b42ee9dbff4f301bc5b731c49
Володимир Сидор <vovansw@ukr.net>
2021-11-22 13:38:15 +0200

    HW8 Additional

---
+++ HW8 add/Loops1.js
@@ -0,0 +1,122 @@
+// Рівносторонній трикутник
+
+// let number = +prompt('Введіть число');
+// let result = '';
+// for (let i = number; i >= 1; i--) {
+//     for (let a = i; a >= 1; a--) {
+//         result = result + ' ';
+//     }
+
+//     for (let c = 1; c >= 1; c--) {
+//         result = result + '*';
+//     }
+
+//     for (let b = (number++) - i; b >= 1; b--) {
+//         result = result + '*';
+//     }
+//     console.log(result);
+//     result = '';
+// }
+
+// Ромб
+
+// let number = +prompt('Введіть число');
+// let result = '';
+// for (let i = number; i >= 1; i--) {
+//     for (let a = i; a >= 1; a--) {
+//         result = result + ' ';
+//     }
+
+//     for (let c = 1; c >= 1; c--) {
+//         result = result + '*';
+//     }
+
+//     for (let b = (number++) - i; b >= 1; b--) {
+//         result = result + '*';
+//     }
+//     console.log(result);
+//     result = '';
+// }
+
+// for (let i = number - 2; i >= 2; i--) {
+//     for (let b = (number--) - i; b >= 1; b--) {
+//         result = result + ' ';
+//     }
+
+//     for (let a = (i--) - 1; a >= 1; a--) {
+//         result = result + '*';
+//     }
+
+//     console.log(result);
+//     result = '';
+// }
+
+// Ромб в рамці
+
+// let number = +prompt('Введіть число');
+// let number1 = number;
+// let result = '';
+// for (let a = (number + (number - 1)) + 6; a >= 1; a--) {
+//     result = result + '*';
+// }
+// console.log(result);
+// result = '';
+
+// for (let i = number; i >= 1; i--) {
+//     for (let c = 1; c >= 1; c--) {
+//         result = result + '*';
+//     }
+
+//     for (let a = i + 1; a >= 1; a--) {
+//         result = result + ' ';
+//     }
+
+//     for (let c = 1; c >= 1; c--) {
+//         result = result + '*';
+//     }
+
+//     for (let b = (number++) - i; b >= 1; b--) {
+//         result = result + '*';
+//     }
+
+//     for (let d = i + 1; d >= 1; d--) {
+//         result = result + ' ';
+//     }
+
+//     for (let f = 1; f >= 1; f--) {
+//         result = result + '*';
+//     }
+//     console.log(result);
+//     result = '';
+// }
+
+// for (let i = number - 2; i >= 2; i--) {
+//     for (let c = 1; c >= 1; c--) {
+//         result = result + '*';
+//     }
+
+//     for (let b = (number + 1) - i; b >= 1; b--) {
+//         result = result + ' ';
+//     }
+
+//     for (let a = (i--) - 1; a >= 1; a--) {
+//         result = result + '*';
+//     }
+
+//     for (let d = (number--) - i; d >= 1; d--) {
+//         result = result + ' ';
+//     }
+
+//     for (let f = 1; f >= 1; f--) {
+//         result = result + '*';
+//     }
+
+//     console.log(result);
+//     result = '';
+// }
+
+// for (let a = (number1 + (number1 - 1)) + 6; a >= 1; a--) {
+//     result = result + '*';
+// }
+
+// console.log(result);
